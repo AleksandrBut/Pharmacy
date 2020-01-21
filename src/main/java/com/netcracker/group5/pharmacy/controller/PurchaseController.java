c6b044335fd21500293678d7b62fe00ddd01a025
AleksandrBut <alex638000@gmail.com>
2020-01-21 22:39:02 +0200

    configure cors privacy

--- src/main/java/com/netcracker/group5/pharmacy/controller/PurchaseController.java
+++ src/main/java/com/netcracker/group5/pharmacy/controller/PurchaseController.java
@@ -4,11 +4,13 @@ import com.netcracker.group5.pharmacy.service.PurchaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+@CrossOrigin
 @Controller
 public class PurchaseController {
 
